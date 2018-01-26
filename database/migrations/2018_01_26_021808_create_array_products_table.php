b34d54dab71178ba71632b3a69e7c910d89b2c5e
kroer84 <kroer17@hotmail.com>
2018-01-26 02:20:40 -0600

    ArrayProduct (modelo, migracion)

---
+++ database/migrations/2018_01_26_021808_create_array_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArrayProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('array_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('array_id')->unsigned();
+			$table->foreign('array_id')->references('id')->on('arrays')->onUpdate('cascade');
+			$table->integer('product_id')->unsigned();
+			$table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade');
+            $table->integer('Cantidad');
+            $table->string('Lugar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('array_products');
+    }
+}
