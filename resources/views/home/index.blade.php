98044dfd93a95ca1908f7c46e86609e9ac1bba5a
Jjx51 <jjx51@outlook.com>
2018-02-24 16:49:43 -0600

    Vista home.index depurada

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,41 +1,47 @@
 @extends('layouts.app')
 
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/misestilos.css') }}">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-md-offset-3 ">
-                    <a type="button" class="btn btn-default btn-lg btn-block letra150" href="#"><h3>Venta en el Mostrador</h3> </a>
+            <a type="button" class="btn btn-default btn-lg btn-block letra150" href="#"><h3>Venta en el Mostrador</h3> </a>
         </div>
     </div>
     
     <div class="row">
-                <form>
-                    <div class="form-group">
-                        <label for="inputcodigo" class="col-lg-1 col-md-1 col-sm-1 control-label text-center letra150 margen-superior margen-derecho margen-izquierdo">Código</label>
-                        <div class="col-lg-9 col-md-9 col-sm-7 col-lg-offset-0 col-md-offset-0 col-sm-offset-1 margen-superior">
-                            <input type="text" id="inputcodigo" class="form-control" placeholder="Ingresa aquí el código">
-                        </div> 
-                        <div class="col-lg-1 col-md-1 col-sm-1">
-                            <button type="submit" class="btn btn-default margen-izquierdo margen-superior text-center">Agregar</button>
-                        </div>   
-                    </div>
-                </form>
+        <form>
+            <div class="form-group">
+                <label for="inputcodigo" class="col-lg-1 col-md-1 col-sm-1 control-label text-center letra150 margen-superior2 margen-derecho margen-izquierdo">Código</label>
+
+                <div class="col-lg-9 col-md-9 col-sm-7 col-lg-offset-0 col-md-offset-0 col-sm-offset-1 margen-superior2">
+                    <input type="text" id="inputcodigo" class="form-control" placeholder="Ingresa aquí el código">
+                </div> 
+
+                <div class="col-lg-1 col-md-1 col-sm-1">
+                    <button type="submit" class="btn btn-default margen-izquierdo margen-superior2 text-center">Agregar</button>
+                </div>   
+                
+            </div>
+        </form>
     </div>
 
     <div class="row margen-superior">
-        <div class="col-lg-4 col-md-4 col-sm-4 margen-superior">
+        <div class="col-lg-4 col-md-4 col-sm-4 margen-superior2">
             <button onclick="Venta()" type="button" class="btn btn-default btn-lg btn-block">Venta</button>
         </div> 
                 
-        <div class="col-lg-4 col-md-4 col-sm-4 margen-superior">
+        <div class="col-lg-4 col-md-4 col-sm-4 margen-superior2">
             <button onclick="Pedido()" type="button" class="btn btn-default btn-lg btn-block">Pedido</button>
         </div>
 
-        <div class="col-lg-4 col-md-4 col-sm-4 margen-superior">
+        <div class="col-lg-4 col-md-4 col-sm-4 margen-superior2">
             <button onclick="Mostrador()" type="button" class="btn btn-default btn-lg btn-block">Mostrador</button>
         </div>
     </div>
-    
 
     <div id="cambia-contenido">
             
