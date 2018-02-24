32227e257c13bb6e3087b26ecbf8453d259db542
Jjx51 <jjx51@outlook.com>
2018-02-24 17:24:22 -0600

    home.venta depurado

--- resources/views/home/venta.blade.php
+++ resources/views/home/venta.blade.php
@@ -1,28 +1,29 @@
-
 <h3 class="text-center">Nombre del Producto</h3>
 
 <div class="borde margen-inferior">
-<div class="row margen-inferior margen-superior">
-	   <div class="col-lg-12 col-md-12 col-sm-12 margen-superior">
-	    	
-		 <div class="col-lg-4 col-md-4 col-sm-4">
-			<img src="img/ejemplo.jpeg " width="100%" alt="" class="img-rounded img-responsive">
-		</div>
+	<div class="row margen-inferior margen-superior">
+		   <div class="col-lg-12 col-md-12 col-sm-12 margen-superior">
 
-		<div class="col-lg-8 col-md-8 col-sm-8">
-			<p>Descripcion</p>
-			<textarea class="form-control" rows="9"></textarea>
-		</div>
-	   </div>
-</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 ">
+					<img src="img/ejemplo.jpeg " width="100%" alt="" class="img-rounded img-responsive imagen-home">
+				</div>
+				<div class="col-lg-8 col-md-8 col-sm-8">
+					<p>Descripcion</p>
+					<textarea class="form-control" rows="9"></textarea>
+				</div>
+		   </div>
+	</div>
 	
-<div class="row">
-	<div class="col-md-12 margen-inferior">
-		<div class="col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-5 col-xs-offset-1">
-		<a href="#" type="button" class="btn btn-default btn-block margen-inferior">Cancelar</a>
-	    </div>
-	   <div class="col-lg-3 col-md-3 col-sm-4 col-xs-5 ">
-		<button type="button" class="btn btn-default btn-block margen-inferior">Añadir</button>
-	   </div>
-	  </div>	
+	<div class="row">
+		<div class="col-md-12 margen-inferior">
+
+			<div class="col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-5 col-xs-offset-1">
+				<a href="#" type="button" class="btn btn-default btn-block margen-inferior">Cancelar</a>
+		    </div>
+			
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-5 ">
+				<button type="button" class="btn btn-default btn-block margen-inferior">Añadir</button>
+			</div>
+		</div>	
+	</div>
 </div>
