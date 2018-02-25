e7190eaa7062475df1a95a5d043f0cfcc1b42c87
Jjx51 <jjx51@outlook.com>
2018-02-25 10:46:44 -0600

    Alerta validada en blade home para aparecer unicamente cuando se necesite

--- resources/views/partials/alerts/general.blade.php
+++ resources/views/partials/alerts/general.blade.php
@@ -1,4 +1,9 @@
-<div class="alert alert-{{ $tipoAlerta }} alert-dismissible">
-  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  <strong>{{ $titulo }}</strong> {{ $mensaje }}
-</div>
+@if(!empty ($tipoAlerta))
+	<div class="row">
+	    <br>           
+		<div class="alert alert-{{ $tipoAlerta }} alert-dismissible">
+		  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		  <strong>{{ $titulo }}</strong> {{ $mensaje }}
+		</div>
+	</div>
+@endif
