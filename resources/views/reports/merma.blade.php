0803abf2af392323f2c0506cb7d1fb07fcc816fe
Jjx51 <jjx51@outlook.com>
2018-02-24 20:54:38 -0600

    Identacion de los blade de los reportes

--- resources/views/reports/merma.blade.php
+++ resources/views/reports/merma.blade.php
@@ -4,24 +4,23 @@
 	<meta charset="UTF-8">
 	<title>Reporte de merma</title>
     <link rel="stylesheet" type="text/css" href="css/pdf.css" media="all">
-	  
 </head>
+
 <body>
 <header class="clearfix">
-      <div class="center">
-        <img id="logo" src="img/pdf/logo.jpeg">
-      </div>
-      <h1 class="empresa">REPORTE DE MERMA</h1>
+  <div class="center">
+    <img id="logo" src="img/pdf/logo.jpeg">
+  </div>
+  
+  <h1 class="empresa">REPORTE DE MERMA</h1>
       <table id="letra" class="table">
-                  <thead>
-                    <tr class="text-center">
-                      <th class="ancho50">FECHA INICIO: 12/05/2017</th>
-                      <th class="ancho50">FECHA FINAL: 21/05/2017</th>
-                    </tr>                    
-                  </thead>
-      </table>
-    
-      
+        <thead>
+          <tr class="text-center">
+            <th class="ancho50">FECHA INICIO: 12/05/2017</th>
+            <th class="ancho50">FECHA FINAL: 21/05/2017</th>
+          </tr>                    
+        </thead>
+      </table> 
 </header>
 <!--TABLA VENTAS-->
 <div class="col-md-12">
@@ -87,37 +86,34 @@
 </div>
 <!--FIN TABLA VENTAS-->
 <div class="col-md-12">
-              <div class="box">
-               <!-- TABLA DESPERDICIADO-->
-                <div class="box-header with-border">
-                  <h3 class="box-title text-center">Lo más desperdiciado</h3>
-                </div><!-- /.box-header -->
-                <div class="box-body">
-                  <table class="table table-bordered">
-                  <thead>
-                    <tr class="text-center">
-                      <th class="ancho50">NOMBRE DEL PRODUCTO</th>
-                      <th class="ancho50">CANTIDAD DESPERDICIADA</th>
-                    </tr>                    
-                  </thead>
-                    <tbody>
-                    <tr class="text-center">
-                      <td class="ancho50">Rosas</td>
-                      <td class="ancho50">300</td>
-                      
-                    </tr>
-                    </tbody>
-                  </table>
-                  <!--FIN TABLA DESPERDICIADO-->
-                </div><!-- /.box-body -->
-                <div class="box-footer clearfix">
-                  
-                </div>
-              </div><!-- /.box -->
+  <div class="box">
+  <!-- TABLA DESPERDICIADO-->
+    <div class="box-header with-border">
+      <h3 class="box-title text-center">Lo más desperdiciado</h3>
+    </div><!-- /.box-header -->
+
+    <div class="box-body">
+      <table class="table table-bordered">
+      <thead>
+        <tr class="text-center">
+          <th class="ancho50">NOMBRE DEL PRODUCTO</th>
+          <th class="ancho50">CANTIDAD DESPERDICIADA</th>
+        </tr>                    
+      </thead>
+        <tbody>
+        <tr class="text-center">
+          <td class="ancho50">Rosas</td>
+          <td class="ancho50">300</td>
+        </tr>
+        </tbody>
+      </table>
+      <!--FIN TABLA DESPERDICIADO-->
+    </div><!-- /.box-body -->
+
+    <div class="box-footer clearfix"></div>
+  </div><!-- /.box -->
 
 </div>
-
-	
 </body>
 </html>
 
--- resources/views/reports/ventaymerma.blade.php
+++ resources/views/reports/ventaymerma.blade.php
@@ -3,26 +3,27 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Reporte de Venta y Merma</title>
-    <link rel="stylesheet" type="text/css" href="css/pdf.css" media="all">
-	  
+  <link rel="stylesheet" type="text/css" href="css/pdf.css" media="all">  
 </head>
+
 <body>
 <header class="clearfix">
-      <div class="center">
-        <img id="logo" src="img/pdf/logo.jpeg">
-      </div>
-      <h1 class="empresa">REPORTE DE VENTA Y MERMA</h1>
-      <table id="letra" class="table">
-                  <thead>
-                    <tr class="text-center">
-                      <th class="ancho50">FECHA INICIO: 12/05/2017</th>
-                      <th class="ancho50">FECHA FINAL: 21/05/2017</th>
-                    </tr>                    
-                  </thead>
-      </table>
-    
-      
+  <div class="center">
+    <img id="logo" src="img/pdf/logo.jpeg">
+  </div>
+
+  <h1 class="empresa">REPORTE DE VENTA Y MERMA</h1>
+
+  <table id="letra" class="table">
+    <thead>
+      <tr class="text-center">
+        <th class="ancho50">FECHA INICIO: 12/05/2017</th>
+        <th class="ancho50">FECHA FINAL: 21/05/2017</th>
+      </tr>                    
+    </thead>
+  </table>    
 </header>
+
 <!--TABLA -->
 <div class="col-md-12">
               <div class="box">
@@ -92,61 +93,63 @@
 
             </div>
 <!--FIN TABLA -->
-<!--INFORME GENERAL-->
-<div class="col-md-12">
-              <div class="box">
-                <div class="box-header with-border">
-                  <h3 class="box-title text-center">INFORME GENERAL</h3>
-                </div><!-- /.box-header -->
-                <div class="box-body">
-                  <table class="table table-bordered">
-                  <thead>
-                    <tr class="text-center">
-                      <th class="ancho25">Lo más vendido</th>
-                      <th class="ancho25"></th>
-                      <th class="ancho25">Lo más desperdiciado</th>
-                      <th class="ancho25"></th>
-                    </tr>                    
-                  </thead>
-                    <tbody>
-                    <tr class="text-center">
-                      <td class="ancho25">NOMBRE DEL PRODUCTO</td>
-                      <td class="ancho25">CANTIDAD VENDIDA</td>
-                      <td class="ancho25">NOMBRE DEL PRODUCTO</td>
-                      <td class="ancho25">CANTIDAD VENDIDA</td>
-                    </tr>           
-                    <tr class="text-center">
-                      <td class="ancho25">Rosas</td>
-                      <td class="ancho25">300</td>
-                      <td class="ancho25">Girasol</td>
-                      <td class="ancho25">280</td>
-                    </tr>
-                    </tbody>
-                  </table>
-                  <table class="table table-bordered">
-                  <thead>
-                    <tr class="text-center">
-                      <th class="ancho50">Costo de Envio</th>
-                      <th class="ancho50">$500.00</th>
-                    </tr>                    
-                  </thead>
-                    <tbody>
-                    <tr class="text-center">
-                      <td class="ancho50 letra-negra">Total Vendido</td>
-                      <td class="ancho50 letra-negra">$4500.00</td>
-                    </tr>
-                    </tbody>
-                  </table><!--FIN INFORME GENERAL-->
-                </div><!-- /.box-body -->
-                <div class="box-footer clearfix">
-                  
-                </div>
-              </div><!-- /.box -->
 
-</div>
+  <!--INFORME GENERAL-->
+  <div class="col-md-12">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title text-center">INFORME GENERAL</h3>
+      </div><!-- /.box-header -->
 
-	
-</body>
+      <div class="box-body">
+        <table class="table table-bordered">
+          <thead>
+            <tr class="text-center">
+              <th class="ancho25">Lo más vendido</th>
+              <th class="ancho25"></th>
+              <th class="ancho25">Lo más desperdiciado</th>
+              <th class="ancho25"></th>
+            </tr>                    
+          </thead>
+          <tbody>
+            <tr class="text-center">
+              <td class="ancho25">NOMBRE DEL PRODUCTO</td>
+              <td class="ancho25">CANTIDAD VENDIDA</td>
+              <td class="ancho25">NOMBRE DEL PRODUCTO</td>
+              <td class="ancho25">CANTIDAD VENDIDA</td>
+            </tr>           
+            <tr class="text-center">
+              <td class="ancho25">Rosas</td>
+              <td class="ancho25">300</td>
+              <td class="ancho25">Girasol</td>
+              <td class="ancho25">280</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-bordered">
+          <thead>
+            <tr class="text-center">
+              <th class="ancho50">Costo de Envio</th>
+              <th class="ancho50">$500.00</th>
+            </tr>                    
+          </thead>
+            <tbody>
+            <tr class="text-center">
+              <td class="ancho50 letra-negra">Total Vendido</td>
+              <td class="ancho50 letra-negra">$4500.00</td>
+            </tr>
+            </tbody>
+        </table><!--FIN INFORME GENERAL-->
+      </div><!-- /.box-body -->
+
+      <div class="box-footer clearfix"></div>
+    </div><!-- /.box -->
+
+  </div>
+
+  	
+  </body>
 </html>
 
 
