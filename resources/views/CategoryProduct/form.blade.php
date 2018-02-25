d5de92f5fc7776c9d7ecd738cdc49ea5af256317
kroer84 <kroer17@hotmail.com>
2018-02-25 17:06:49 -0600

    Creacion de formulario para CategoryProduct

---
+++ resources/views/CategoryProduct/form.blade.php
@@ -0,0 +1,19 @@
+{!! Form::open(['url' => $url,'method' => $method]) !!}
+  	 
+		<div class="form-group">
+			<div class="col-sm-3 col-xs-12">
+				{{Form::label('name', 'Nombre de la categoria')}}
+			</div>
+			<div class="col-sm-9 col-xs-12">
+				{{ Form::text('name', $CategoryProduct->name,['class'=>'form-control','placeholder'=>'Nombre de la categoria...'])}}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-12">
+			<br>
+				<div class="form-group text-right">
+					{{ Form::submit('Crear',['class'=>'btn btn-primary btn-block']) }}
+				</div>
+			</div>
+		</div>
+{!! Form::close() !!}
