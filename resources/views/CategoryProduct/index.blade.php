3a9c5e560818cbcdc1000ac0e546ececc45308e4
kroer84 <kroer17@hotmail.com>
2018-02-25 13:08:17 -0600

    CategoryProduct medoto index

---
+++ resources/views/CategoryProduct/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('head')
+	<link rel="stylesheet" href="{{ asset('css/misestilos.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
+	<link rel="stylesheet" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+@endsection
+
+
+@section('content')
+	<div class="container">
+		<h1 class="text-center">Lista de Categorias</h1>
+	</div>
+	<div class="row row margen-superior margen-inferior">
+  			<div class="col-sm-offset-4 col-sm-4">
+			  	<a href="{{ route('Categoryp.create') }}" class="btn btn-success btn-lg btn-block">Nueva Categoria</a>
+		    </div>
+	</div>
+	<div class="container">
+  		<div class="row ">
+  			<div class="table-responsive color-tabla margen-superior">
+			  	<table id="example" class="table table-striped  text-center " cellspacing="0" width="100%">
+			        <thead>
+			            <tr>
+			                <th class="text-center">N°</th>
+			                <th class="text-center">Nombre</th>
+			                <th class="text-center">Acciones</th>
+			            </tr>
+			        </thead>
+			        <tbody>
+						
+						@foreach ($categorias as $categoria)
+							<tr>
+								<td>{{ $categoria->id }}</td>
+								<td>{{ $categoria->name }}</td>
+								<td> <a href="" class="btn btn-primary">editar</a> <a href="" class="btn btn-danger">Eliminar</a></td>
+							
+							</tr>
+						@endforeach
+							
+			        	
+			        </tbody>
+			    </table>
+			</div>
+  		</div>
+  		
+  	</div>
+
+
+@endsection
+
+@section('javascript')
+	<script src="//code.jquery.com/jquery-1.12.4.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+
+	<script type="text/javascript">
+        $(document).ready(function() {
+        $('#example').dataTable( {
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+                }
+            } );
+        } );
+    </script>
+
+    <script type="text/javascript" src="{{ asset('js/lightbox.js') }}"></script>
+
+
+@endsection
