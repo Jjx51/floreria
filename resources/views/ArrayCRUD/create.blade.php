9199f54a2fa904532f3105d222bc2ddb9e28631c
Jjx51 <jjx51@outlook.com>
2018-03-15 17:46:42 -0600

    Estilo dado a la vista array.edit

--- resources/views/ArrayCRUD/create.blade.php
+++ resources/views/ArrayCRUD/create.blade.php
@@ -17,7 +17,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12 col-md-offset-0">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading text-center lead"><strong>Crear nuevo arreglo de flores</strong></div>
                         <div class="panel-body">
