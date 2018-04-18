67be4e16b56b7ec7096fd225885df212ed8af819
Jjx51 <jjx51@outlook.com>
2018-04-17 23:06:33 -0500

    Arreglado bug donde el administrador iniciaba sesion al crear un usuario

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -72,6 +74,20 @@ class RegisterController extends Controller
             'rol'=> 'user',
         ]);
     }
+
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        /*$this->guard()->login($user);*/
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath())->with('success','Usuario creado exitosamente');
+    }
+
+
 
 
 }
